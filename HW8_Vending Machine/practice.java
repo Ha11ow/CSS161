be6ed44711b4932b71d4cd4b80ad046a44a1cd84
Ben Khabazan <52260208+Ha11ow@users.noreply.github.com>
2020-04-04 16:29:22 -0700

    Delete practice.java

--- HW8_Vending Machine/practice.java
+++
@@ -1,9 +0,0 @@
-public class practice
-{
-    public static void main (String Args[]){
-        int[] a = {1,2,3,4,5};
-        int[] b = {6,7,8,9};
-        b[2] = a[3];
-        System.out.println("b[2] is " + b[2]);
-    }
-}
